0e662a671f17e4f481553c046137fb60d3fad5db
Michael Hwang <mhwang@paraport.com>
2020-07-29 22:29:08 +0000

    Fix invalid header value
    Log the invalid header value and keep processing.
    
    Issues: WAM-91

--- lib/logstash/inputs/imap.rb
+++ lib/logstash/inputs/imap.rb
@@ -266,27 +266,33 @@ class LogStash::Inputs::IMAP < LogStash::Inputs::Base
 
       # Add fields: Add message.header_fields { |h| h.name=> h.value }
       mail.header_fields.each do |header|
-        # 'header.name' can sometimes be a Mail::Multibyte::Chars, get it in String form
-        name = @lowercase_headers ? header.name.to_s.downcase : header.name.to_s
-        # Assume we already processed the 'date' above.
-        next if name == "Date"
+        begin
+          # 'header.name' can sometimes be a Mail::Multibyte::Chars, get it in String form
+          name = @lowercase_headers ? header.name.to_s.downcase : header.name.to_s
+          # Assume we already processed the 'date' above.
+          next if name == "Date"
 
-        # Call .decoded on the header in case it's in encoded-word form.
-        # Details at:
-        #   https://github.com/mikel/mail/blob/master/README.md#encodings
-        #   http://tools.ietf.org/html/rfc2047#section-2
-        value = transcode_to_utf8(header.decoded.to_s)
+          # Call .decoded on the header in case it's in encoded-word form.
+          # Details at:
+          #   https://github.com/mikel/mail/blob/master/README.md#encodings
+          #   http://tools.ietf.org/html/rfc2047#section-2
+          value = transcode_to_utf8(header.decoded.to_s)
 
-        case (field = event.get(name))
-        when String
-          # promote string to array if a header appears multiple times
-          # (like 'received')
-          event.set(name, [field, value])
-        when Array
-          field << value
-          event.set(name, field)
-        when nil
-          event.set(name, value)
+          case (field = event.get(name))
+          when String
+            # promote string to array if a header appears multiple times
+            # (like 'received')
+            event.set(name, [field, value])
+          when Array
+            field << value
+            event.set(name, field)
+          when nil
+            event.set(name, value)
+          end
+        rescue => e
+          # Log and try to keep going
+          # REVIEW: Do we need to make sure the critical headers get captured at least?
+          @logger.warn("Issue parsing a header field: #{header.name.to_s}, #{mail.message_id}")
         end
       end
 
